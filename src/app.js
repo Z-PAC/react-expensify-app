56fe1283280f6865b887f4dcb8305d12a2d847a6
Z-PAC <jpc_ace@hotmail.com>
2018-09-27 18:37:36 +0100

    Adds Expensify styles

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { firebase } from './firebase/firebase';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+
 import AppRouter, { history } from './routers/AppRouter.js';
 import configureStore from './store/configureStore';
 
 import { startSetExpenses } from './actions/expenses';
 import { login, logout } from './actions/auth';
 
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
-
-import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 
@@ -29,7 +31,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -18,11 +18,24 @@ export class EditExpensePage extends React.Component {
   render() {
     return (
       <div>
-        <ExpenseForm
-          expense={this.props.expense}
-          onSubmit={this.onSubmit}
-        />
-        <button onClick={this.onClick}>Remove</button>
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Edit Expense</h1>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm
+            expense={this.props.expense}
+            onSubmit={this.onSubmit}
+          />
+          <button
+            className="button button--secondary"
+            onClick={this.onClick}
+          >
+            Remove Expense
+          </button>
+        </div>
+
       </div>
     );
   }
@@ -30,13 +43,13 @@ export class EditExpensePage extends React.Component {
 
 const mapStateToProps = (state, props) => {
   return {
-    expense: state.expenses.find((expense) => expense.id === props.match.params.id )
+    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    startEditExpense: (id,expense) => dispatch(startEditExpense(id,expense)),
+    startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
     startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
   };
 };
